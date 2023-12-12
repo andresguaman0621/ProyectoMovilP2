1107ddb3041b65b91a0627c368d0069b74e769c0
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2023-12-11 23:46:21 -0500

    Cambio de estructura TabbedPage a TabBar

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -3,7 +3,7 @@ using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace ProyectoMovilP2;
 
-public partial class Home : TabbedPage
+public partial class Home : ContentPage
 {
 	public Home()
 	{
